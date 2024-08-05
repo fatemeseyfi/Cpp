741d9f000842ed16ec49dbbf0ecdcca7afbfe645
fatemeseyfi <fatemeazs2000@gmail.com>
2024-08-05 21:42:00 +0330

    d

--- Final/ICircle.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Elipse.h"
-class ICircle : virtual public IElipse {
-public:
-
-	virtual void setRadius(const float& radius) = 0;
-	virtual float getRadius() const = 0;
-
-};
--- Final/IDrawable.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <SDL.h>
-
-class IDrawable {
-public:
-	virtual void draw(SDL_Renderer* renderer)  = 0;
-};
